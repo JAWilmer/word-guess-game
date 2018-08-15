06d55e7ae034b88782e76ea0ac5297e25f279d59
JAWilmer <39922930+JAWilmer@users.noreply.github.com>
2018-08-15 12:36:53 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,52 @@
- // Create variables
+window.onload = function () {
 
- // FUNCHTIONS
+// CREATE VARIBLES
+let player; 
+let parkName = ""; //array
+let randomPark; //park selected from array
+let parkLetter; //letters in park name
+let letterCount; // number of letters from randomly selected park
+let letterGuessed; //letter guessed
+let letterCountRemaining;  
 
 
- // Array of words for word guess
- let game = [“Acadia”, “Arches”, “Badlands”, “Big Bend”, “Crater Lake”, “Death Valley”, “Denali”, “Everglades”, “Glacier”, “Grand Canyon”, “Grand Teton”, “Great Smoky Mountains”, “Hawaiʻi Volcanoes” “Joshua Tree”, “Mammoth Cave”, “Mesa Verde”, “Olympic”, “Petrified Forest”, “Redwood”, “Rocky Mountain”, “Saguaro”, “Sequoia”, “Shenandoah”, “Wind Cave”, “Yellowstone”, “Yosemite”, “Zion”];
- let randomLetter =“”;
- var person; 
- var myScore=0;
- var computerScore=0;
+// CREATE ARRAY OF PARKS FOR WORD GUESS
+let parkName = ["Acadia", "Arches", "Badlands", "Big Bend", "Crater Lake", "Death Valley", "Denali", "Everglades", "Glacier", "Grand Canyon", "Grand Teton", "Great Smoky Mountains", "Hawaiʻi Volcanoes" "Joshua Tree", "Mammoth Cave", "Mesa Verde", "Olympic", "Petrified Forest", "Redwood", "Rocky Mountain", "Saguaro", "Sequoia", "Shenandoah", "Wind Cave", "Yellowstone", "Yosemite", "Zion"];
+
+//
+
+//CREATE FUNCIONS
+
+//STEPS TO SHOW LETTER BLANKS ON SCREEN 
+//randomly retrieve name
+let randomPark = parkName[Math.floor(Math.random() * parkName.length)]; 
+//calculate number blanks
+// write blanks on screen
+letterCount
+// determine how many letter guesses player has based on lenght of word/ words
 
 
-//generate a random number from 0-2
-while (myScore < 5  && computerScore < 5) {
-        person = prompt(“Hello let's play rock, paper scissors: Enter R P S“);
-        person=person.toUpperCase();
-        randomLetter=game[Math.floor(Math.random() * 3)];
-        alert(“Computer's Response is “+ randomLetter);
-        if(person===“R“ && (randomLetter===“P“||randomLetter===“S“)){
-            myScore++;
-            alert(“Computer's Loses This Round! You Win. Your score is  “+ myScore + “ Computer's score is “+ computerScore);
-        } 
-        else if(person===“P“ && randomLetter===“R“) {
-            myScore++;
-            alert(“Computer's Loses This Round! You Win. Your score is  “+ myScore + “ Computer's score is “+ computerScore);
-        }
-        else if(person===“S“ && randomLetter===“P“) {
-            myScore++;
-            alert(“Computer's Loses This Round! You Win. Your score is  “+ myScore + “ Computer's score is “+ computerScore);
-        } 
-        else if(person===randomLetter) {
-            alert(“No one wins this round! Your score is  “+ myScore+ “ Computer's score is “+ computerScore);
-        }
-        else{
-            computerScore++;
-            alert(“You Lose This Round!  The Computer's score is  “+ computerScore + “ and your score is “+ myScore);
+//PLACE LETTER GUESSES WHERE THEY BELONG 
+// place correct guesses in correct spot or decrease guess count
+if (letterGuessed === parkLetter) {
+    //letter is placed in correct spot
+}
+else if (letterGuessed !== parkLetter) { 
+    //incorrect letter guess is placed in box
+}
+while 
+    // there are spaces to be filled or there are guesses to be made
 
-        }
-//scoring
-        if(computerScore> myScore){
-            alert(“*** you lose ***“);
-        }else{
-            alert(“************You Win. Your score is  “+ myScore + “Computer's score is “+ computerScore);
-        }
-    
-    myScore=0;
-    computerScore=0;
+
+
+// SHOW REMAINING NUMBER OF GUESSES
+
+
+
+// GIVE HINTS IF GUESSED 3 WRONG IN A ROW
+
+//CONGRATULATE WINNER
+//Show video/ facts/ pictures of park
+
+}  // closing for function onload
+
