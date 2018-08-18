ae5e98f5bbb1c684d6e8ab4ab08772f7db33ba29
JAWilmer <39922930+JAWilmer@users.noreply.github.com>
2018-08-18 06:39:54 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,143 +1,145 @@
 window.onload = function () {
 
-    // CREATE VARIBLES
-    let park; // Array of parks
-    let selectedPark; //Park selected from array  (// chosenCategory)
-    let alphabet; //Array of letters to choose
-    let letters; //buttons that hold letters from array
-    let space; //Number of spaces in word
-    let guess; //Letter guessed
-    let guesses = []; // Stored guesses
-    let counter; //Count correct guesses
-    let remaining; //Guesses remaining 
-    let hint; //Array of hints 
-    let clue; //Not sure if I need this -  this is the clue button
+//VARIABLES
+  var parks;  // Array of parks 
+  var chosenPark;  // Randomly chosen park 
+  var abc; // Alphabet buttons 
+  var getHint;  // Word getHint
+  var word;  // Word 
+  var letterGuess;  // Letter user guesses 
+  var storedGuesses = [ ];  // Stores user's letter guesses 
+  var letterCount;  // Letter count 
+  var counter;  // Counts correct guesses
+  var space;  // Number of spaces in park '*'
 
+// RETRIEVE ELEMENTS FROM DOCUMENT
+  var showLetterCount = document.getElementById("myLetterCount");
+  var getHint = document.getElementById("hint");
+  var showClue = document.getElementById("clue");
+  var showWinCount = document.getElementById("winCount")
+  var showLossCount = document.getElementById("lossCount")
 
-    // CREATE ARRAYS
-    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// CREATE ALPHABET UL
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "'"];        
 
-    let park = ["Acadia", "Arches", "Badlands", "Big Bend", "Crater Lake", "Death Valley", "Denali", "Everglades", "Glacier", "Grand Canyon", "Grand Teton", "Great Smoky Mountains", "Hawaiʻi Volcanoes" "Joshua Tree", "Mammoth Cave", "Mesa Verde", "Olympic", "Petrified Forest", "Redwood", "Rocky Mountain", "Saguaro", "Sequoia", "Shenandoah", "Wind Cave", "Yellowstone", "Yosemite", "Zion"];
-
-    let hint = ["Features a famous lighthouse in Maine."]; // have to add more hints that line up with parks
-
-    // GET ELEMENTS FROM DOC --   do I need more of these?
-    let remaining = document.querySelector("remaining");
-    let clue = document.querySelector("clue");
-    let hint = document.querySelector("hint");
-
-    //CREATE FUNCIONS
-
-    // create alphabet ul  --- this is a mess!  my key words are all messed up!
-    let letters = function () {
-        myletters = document.querySelector("letters");
-         = document.createElement("ul");
+  var abc = function () {
+    myAbc = document.getElementById('abc');
+    letters = document.createElement('ul');
 
     for (var i = 0; i < alphabet.length; i++) {
-        letters.id = "alphabet";
-        list = document.createElement("li");
-        list.id = "letter";
-        list.innerHTML = alphabet[i];
-        check();
-        myButtons.appendChild(letters);
-        letters.appendChild(list);
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myAbc.appendChild(letters);
+      letters.appendChild(list);
     }
-}
+  }
 
-//Randomly retrieve park  -  i think this is a mess too!
-result = function () {
-    selectedParkHolder = document.querySelector('hold');
+// CREATE GUESSES UL
+   result = function () {
+    wordHolder = document.getElementById('hold');
     correct = document.createElement('ul');
 
     for (var i = 0; i < word.length; i++) {
-        correct.setAttribute('id', 'my-random-park');
-        guess = document.createElement('li');
-        guess.setAttribute('class', 'guess');
-        if (word[i] === "-") {
-            guess.innerHTML = "-";
-            space = 1;
-        } else {
-            guess.innerHTML = "_";
-        }
-
-        guesses.push(guess);
-        wordHolder.appendChild(correct);
-        correct.appendChild(guess);
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (word[i] === "*") {
+        guess.innerHTML = "*";
+        space = 1;
+      } else {
+        guess.innerHTML = "_";
+      }
+      storedGuesses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
     }
-}
+  }
 
-// Show guesses remaining
-comments = function () {
-    showRemaining.innerHTML = "You have " + remaining + " guesses remaining.";
-    if (remaining < 1) {
-        showRemaining.innerHTML = "Game Over";
-    }
-    for (var i = 0; i < geusses.length; i++) {
-        if (counter + space === geusses.length) {
-            showRemaining.innerHTML = "You Win!";
-        }
-    }
-}
+// SHOW LETTER COUNT
+   comments = function () {
+    showLetterCount.innerHTML = "Guesses Remaining:     " +letterCount;
+    if (letterCount < 1) {
+      showLetterCount.innerHTML = "Game Over";
+      showLossCount.innerHTML = "Losses:" -+ 1;
+      window.location.reload(true);
+      $('#parkModal').modal(show);
 
-// OnClick Function
-check = function () {
+
+    }
+    for (var i = 0; i < storedGuesses.length; i++) {
+      if (counter + space === storedGuesses.length) {
+        showLetterCount.innerHTML = "You Won!";
+        showWinCount.innerHTML = "Wins:" + 1;
+        window.location.reload(true);
+        $('#parkModal').modal(show);
+      }
+    }
+  }
+
+// ONCLICK FUNCTION
+   check = function () {
     list.onclick = function () {
-        let guess = (this.innerHTML);
-        this.setAttribute("class", "active");
-        this.onclick = null;
-        for (var i = 0; i < word.length; i++) {
-            if (word[i] === guess) {
-                guesses[i].innerHTML = guess;
-                counter += 1;
-            }
-        }
-        let w = (word.indexOf(guess));
-        if (w === -1) {
-            remaining -= 1;
-            comments();
-        } else {
-            comments();
-        }
+      var letterGuess = (this.innerHTML);
+      this.setAttribute("class", "active");
+      this.onclick = null;
+      this.style.backgroundColor = "rgb(141, 149, 156)";
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === letterGuess) {
+          storedGuesses[i].innerHTML = letterGuess;
+          counter += 1;
+        } 
+      }
+      var j = (word.indexOf(letterGuess));
+      if (j === -1) {
+        letterCount -= 1;
+        comments();
+      } else {
+        comments();      }
     }
-}
+  }
+    
+  // PLAY
+  play = function () {
+    parks = [
+        ["ACADIA", "ARCHES", "BADLANDS", "BIG*BEND", "BISCAYNE", "CRATER*LAKE", "DEATH*VALLEY", "DENALI", "EVERGLADES", "GLACIER", "GRAND*CANYON", "GRAND*TETON", "GREAT*SMOKY*MOUNTAINS", "HAWAI'I*VOLCANOES", "ISLE ROYALE", "JOSHUA*TREE", "MAMMOTH*CAVE", "MESA*VERDE", "OLYMPIC", "PETRIFIED*FOREST", "REDWOOD", "ROCKY*MOUNTAIN", "SAGUARO", "SEQUOIA*AND*KINGS*CANYON", "SHENANDOAH", "WIND*CAVE", "YELLOWSTONE", "YOSEMITE", "ZION"]
+    ];
 
-// Play
-play = function () {
-    let selectedPark = park[Math.floor(Math.random() * park.length)];
-    word = selectedPark[Math.floor(Math.random() * selectedPark.length)];
+    chosenPark = parks[Math.floor(Math.random() * parks.length)];
+    word = chosenPark[Math.floor(Math.random() * chosenPark.length)];
     word = word.replace(/\s/g, "-");
     console.log(word);
-    letters();
-
-    guesses = [];
-    remaining = 12;
+    abc();
+    storedGuesses = [ ];
+    letterCount = 14;
     counter = 0;
     space = 0;
     result();
     comments();
 }
 
-play();
+  play();
+  
+  // HINT
 
+    hint.onclick = function() {
 
-// Hint
+      hints = [
+        ["Within its borders you will find the tallest mountain on the Atlantic coast.", "This park is known for its more than 2,000 sandstone arches.", "This park contains the largest known collection of late Eocene and Oligocene mammal fossils.", "Know for its arid canyons, this park is one of several parks that sit on the U.S. - Mexico border.  It also encompasses a large part of the Chihuahuan Desert.", "This park has four interrelated marine ecosystems and sits on the north end of the Florida Keys.", "This park is home to the deepest lake in the United States.  It is formed by the caldera of Mt. Mazama.", "This park is the is the hottest, lowest, and driest place in the United States. Daytime temperatures that have exceeded 130 °F (54 °C).", "This park is home to the tallest mountain in North America. ", "This park is comprised of the largest tropical wilderness in the United States.", "Located in the Rocky Mountains, this park includes 26 glaciers and 130 named lakes. ", "Over millions of years, the Colorado River has exposed this park’s grand multi-colored layers.  ", "Home to Jackson Hole, this park also boasts the tallest mountain in its namesake mountain range.", "This park is home to over 400 vertebrate species, 100 tree species, and 5000 plant species.  It’s namesake is a smaller chain located in the southern Appalachian Mountains", "Two of the world's most active geological features are housed within this park. In 2018 the face of the park was forever altered.", "One large island, over 400 smaller islands, shipwrecks, waterways, and hiking trails compose this island in Lake Superior.", "This park stretches thought the Colorado and Mojave Deserts, as well as the Little San Bernardino Mountains. The park features sand dunes, dry lakes, mountains, monoliths, and many of its namesake.", "This park is home to the world’s longest known cave system.", "This park is home to one of the largest Ancestral Puebloan cliff dwelling.", "This park on the Pacific coast is home to the only rainforest in North America. ", "This park features cliffs of bentonite rock. It houses dinosaur follis, 225 million year old fossils and 350+ Native American sites.", "This park is home to the tallest trees on earth. ", "This park straddles the Continental Divide and is home to a scenic road which reaches more than 12,000 feet in elevation.", "Nestled in both the RIncon and Tucson Mountains, this park is home to barrel cacti, chollas, prickly pear, and its namesake.", "You will find General Sherman here, as well as the tallest mountain in the contiguous United States.", "A long and narrow park, you could travel this park by trail or by road, the two run nearly parallel. ", "Below ground, this park is famous for calcite formations called boxwork. Above ground, bison, black-footed ferrets, and prairie dogs still roam. ", "This park is situated on one of the largest calderas on earth.", "This park is home to granite cliffs, old-growth forests, three giant sequoia groves and one of North America's tallest waterfalls at 2,425 feet (739 m) high.", "Much of this park was formed by formed by forks of the Virgin River, which has divided it into four ecosystems: desert, riparian, woodland, and coniferous forest"]
+    ];
 
-clues.onclick = function () {
-    let clueIndex = indexOf(word);
-    showHint.innerHTML = "Clue: Which park " + clues[clueIndex];
-};
+    var parkIndex = parks.indexOf(chosenPark);
+    var hintIndex = chosenPark.indexOf(word);
+    showClue.innerHTML = " " + hints [parkIndex][hintIndex];
+  };
 
+// RESET
+  document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    play();
+  } 
 }
-
-
-
- //Change button color when selected example
-//    var button = document.querySelector('.button');
-
-//button.onclick = function () {
-// var red = Math.floor(Math.random() * 256);
-// var blue = Math.floor(Math.random() * 256);
-// var green = Math.floor(Math.random() * 256);
-
-// this.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
-//};
